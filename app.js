d9d3799fbf8d079f596ac24e029114027cd5f478
Deepak2110704 <717822p211@kce.ac.in>
2025-06-25 11:21:55 +0530

    BULB

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+const on_btn = document.getElementById("on");
+const off_btn = document.getElementById("off");
+
+var img = document.getElementById("bulb");
+
+function bulb_on(){
+    img.src ="https://cdn.pixabay.com/photo/2014/04/03/10/32/bulb-310821_1280.png"
+}
+
+function bulb_off(){
+    img.src ="https://cdn.pixabay.com/photo/2013/04/01/09/21/bulb-98514_640.png"
+}
+
